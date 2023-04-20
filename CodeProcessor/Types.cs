3ad7f5b3bf9238d9c494cf3014a940fcac665faa
Benedek Lellei <lellei.benedek@gmail.com>
2023-04-20 06:55:23 +0100

    fix built-in enum definitions in sem analysis

--- CodeProcessor/Types.cs
+++ CodeProcessor/Types.cs
@@ -53,8 +53,8 @@ namespace CodeProcessor
     public class TypeSystem
     {
         private Dictionary<string, string[]> enumDefinitions = new Dictionary<string, string[]>{
-            {"CARDTYPES", new string[]{"Action, Attack, Reaction, Treasure, Victory"}},
-            {"VISIBILITY", new string[]{"AllVisible, TopVisible, NoneVisible"}}
+            {"CARDTYPES", new string[]{"ACTION", "ATTACK", "REACTION", "TREASURE", "VICTORY"}},
+            {"VISIBILITY", new string[]{"ALLVISIBLE", "TOPVISIBLE", "NONEVISIBLE"}}
         };
 
         public SymbolType this[string[] typeChain]
