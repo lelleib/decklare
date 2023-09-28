00247bd56ef307711cdbeca4dfec186f3fa7043d
Benedek Lellei <lellei.benedek@gmail.com>
2023-09-28 12:11:47 +0200

    add initial program code to generated sample

--- DbgLib.Tests/DominionDbgSampleGenerated.cs
+++ DbgLib.Tests/DominionDbgSampleGenerated.cs
@@ -2,20 +2,227 @@ namespace DbgLib.Tests.DominionDbgSampleGenerated;
 
 using CardPredicate = Predicate<Card>;
 using Number = Int32;
+using Effect = Action;
 
 public class DbgEnvironment : DbgEnvironmentBase
 {
     private Gameplay gamePlay = new Gameplay();
-    public DbgEnvironment(IDbgRuntime runtime) : base(runtime)
-    { }
 
-    public void InitDominion(int playerCount)
+    // public Supply? Supply; // TODO impl
+    public Pile? Trash;
+    public Pile? CenterPile;
+    public Pile? Hand;
+    public Pile? Deck;
+    public Pile? Discard;
+    public Pile? InPlay;
+    public Pile? This;
+    public Number? Action;
+    public Number? Buy;
+    public Number? Coin;
+    public Number? Victory;
+    public Number? Discount;
+    public Player? Me;
+    public Player? ActivePlayer;
+    public Player? LeftPlayer;
+    public Player? RightPlayer;
+    public List<Player>? AllPlayers;
+    public List<Player>? AllOtherPlayers;
+
+    public DbgEnvironment(IDbgRuntime _runtime) : base(_runtime)
+    {
+        void program()
+        {
+            InitDominion
+            (
+            3
+            );
+
+            Repeat1
+            (
+            () =>
+            {
+                For12
+                (
+                AllPlayers?.ToList<PlayerBase>(),
+                () =>
+                {
+                    While12
+                    (
+                    Action > 0,
+                    () =>
+                    {
+                        Let1Choose2From3And4
+                        (
+                        Me,
+                        1,
+                        () =>
+                        {
+                            var card_to_play = NewPile();
+
+                            _1From2And3To4
+                            (
+                            (_fromPile) =>
+                                Let1Choose2Where34
+                                (
+                                Me,
+                                1,
+                                (_x) =>
+                                    ((Card)_x).Types.Contains(CardType.Action),
+                                _fromPile
+                                ),
+                            Hand,
+                            (_it, _toPile) =>
+                                Put12
+                                (
+                                _it,
+                                _toPile
+                                ),
+                            card_to_play
+                            );
+                        },
+                        () =>
+                        {
+                            Break();
+                        }
+                        );
+                    }
+                    );
+
+                    While12
+                    (
+                    Buy > 0,
+                    () =>
+                    {
+                        Let1Choose2From3And4
+                        (
+                        Me,
+                        1,
+                        () =>
+                        {
+                            var card = (Card?) /* TODO impl casting */_1From2And3To4
+                            (
+                            (_fromPile) =>
+                                Let1Choose2Where34
+                                (
+                                Me,
+                                1,
+                                (_x) =>
+                                    ((Card)_x).Cost - Discount < Coin,
+                                _fromPile
+                                ),
+                            Hand,
+                            (_it, _toPile) =>
+                                Put12
+                                (
+                                _it,
+                                _toPile
+                                ),
+                            Discard
+                            );
+
+                            Coin = (Coin - (card?.Cost - Discount));
+                        },
+                        () =>
+                        {
+                            Break();
+                        }
+                        );
+                    }
+                    );
+
+                    _1From2And3To4
+                    (
+                    (_fromPile) =>
+                        TakeAll1
+                        (
+                        _fromPile
+                        ),
+                    Hand,
+                    (_it, _toPile) =>
+                        Put12
+                        (
+                        _it,
+                        _toPile
+                        ),
+                    Discard
+                    );
+
+                    _1From2And3To4
+                    (
+                    (_fromPile) =>
+                        TakeAll1
+                        (
+                        _fromPile
+                        ),
+                    InPlay,
+                    (_it, _toPile) =>
+                        Put12
+                        (
+                        _it,
+                        _toPile
+                        ),
+                    Discard
+                    );
+
+                    _1From2And3To4
+                    (
+                    (_fromPile) =>
+                        Pop12
+                        (
+                        5,
+                        _fromPile
+                        ),
+                    Deck,
+                    (_it, _toPile) =>
+                        Put12
+                        (
+                        _it,
+                        _toPile
+                        ),
+                    Hand
+                    );
+
+                    Action = 1;
+
+                    Buy = 1;
+
+                    Coin = 0;
+
+                    Discount = 0;
+
+                    If12
+                    (
+                    true /*TODO win condition*/,
+                    () =>
+                    {
+                        Break();
+                    }
+                    );
+                }
+                );
+
+                If12
+                (
+                true /*TODO win condition*/,
+                () =>
+                {
+                    Break();
+                }
+                );
+            }
+            );
+        }
+
+        initProgram(program);
+    }
+
+    private void InitDominion(int playerCount)
     { // AllPlayers; Supply, Trash, CenterPile
       // AllOtherPlayers, LeftPlayer, RightPlayer; Action, Buy, Coin, Discount, Victory; Deck, Hand, Discard, InPlay
         gamePlay = new Gameplay();
         for (int i = 0; i < playerCount; i++)
         {
-            gamePlay.AllPlayers.Add(new Player
+            gamePlay.AllPlayers.Add(new Player(this)
             {
                 Action = 1,
                 Buy = 1,
@@ -41,14 +248,22 @@ public class DbgEnvironment : DbgEnvironmentBase
         {
             var player = gamePlay.AllPlayers[playerCount];
             player.AllOtherPlayers = gamePlay.AllPlayers.Where((p, j) => j != i).ToList();
-            player.RightPlayer = playerCount == 0 ? gamePlay.Players.Last() : gamePlay.Players[playerCount - 1];
-            player.LeftPlayer = playerCount == gamePlay.Players.Count - 1 ? gamePlay.Players.First() : gamePlay.Players[playerCount + 1];
+            player.RightPlayer = playerCount == 0 ? gamePlay.AllPlayers.Last() : gamePlay.AllPlayers[playerCount - 1];
+            player.LeftPlayer = playerCount == gamePlay.AllPlayers.Count - 1 ? gamePlay.AllPlayers.First() : gamePlay.AllPlayers[playerCount + 1];
         }
     }
 }
 
 public class Card : CardBase
-{ }
+{
+    public CardName Name { get; set; }
+    public List<CardType> Types { get; set; } = new List<CardType>();
+    public Number Cost { get; set; }
+    public Effect ActionEffect { get; set; } = () => { };
+    public Effect TreasureEffect { get; set; } = () => { };
+    public Effect ReactionEffect { get; set; } = () => { };
+    public Effect VictoryEffect { get; set; } = () => { };
+}
 
 public class Game
 { }
@@ -66,19 +281,63 @@ public class Pile : PileBase
 
 public class Player : PlayerBase
 {
-    public List<Player> AllOtherPlayers { get; set; } = new List<Player>();
-    public Player LeftPlayer { get; set; } = new Player();
-    public Player RightPlayer { get; set; } = new Player();
-    public Number Action { get; set; } = 0;
-    public Number Buy { get; set; } = 0;
-    public Number Coin { get; set; } = 0;
-    public Number Discount { get; set; } = 0;
-    public Number Victory { get; set; } = 0;
-    public Pile Deck { get; set; } = new Pile();
-    public Pile Hand { get; set; } = new Pile();
-    public Pile Discard { get; set; } = new Pile();
-    public Pile InPlay { get; set; } = new Pile();
+    private DbgEnvironment environment;
+
+    public List<Player>? AllOtherPlayers { get; set; }
+    public Player? LeftPlayer { get; set; }
+    public Player? RightPlayer { get; set; }
+    public Number? Action { get; set; }
+    public Number? Buy { get; set; }
+    public Number? Coin { get; set; }
+    public Number? Discount { get; set; }
+    public Number? Victory { get; set; }
+    public Pile? Deck { get; set; }
+    public Pile? Hand { get; set; }
+    public Pile? Discard { get; set; }
+    public Pile? InPlay { get; set; }
+
+    public Player(DbgEnvironment environment)
+    {
+        this.environment = environment;
+    }
+
+    public override void setPlayerContext()
+    {
+        environment.AllOtherPlayers = AllOtherPlayers;
+        environment.LeftPlayer = LeftPlayer;
+        environment.RightPlayer = LeftPlayer;
+
+        environment.Action = Action;
+        environment.Buy = Buy;
+        environment.Coin = Coin;
+        environment.Discount = Discount;
+        environment.Victory = Victory;
+
+        environment.Deck = Deck;
+        environment.Hand = Hand;
+        environment.Discard = Discard;
+        environment.InPlay = InPlay;
+    }
 }
 
 public class Supply : SupplyBase
 { }
+
+public enum CardType
+{
+    Action,
+    Attack,
+    Reaction,
+    Treasure,
+    Victory
+}
+
+public enum CardName
+{ }
+
+public enum Visibility
+{
+    AllVisible,
+    TopVisible,
+    NoneVisible
+}
